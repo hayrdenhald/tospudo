b3dc9efaa1e7e399dd2e07e694390a0be387f98a
hayrdenhald <108034885+hayrdenhald@users.noreply.github.com>
2026-04-05 18:53:04 +0200

    ci: remove NPM token

--- knip.config.ts
+++ knip.config.ts
@@ -3,7 +3,6 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   entry: ["src/cli.ts", "tests/**/*.ts"],
   project: ["src/**/*.ts", "tests/**/*.ts"],
-  ignoreBinaries: ["only-allow"],
 };
 
 export default config;
